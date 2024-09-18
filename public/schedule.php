1b87e4d050c3ecdc5b82e08c38666dc04c710578
yakiv <baiduck2015@gmail.com>
2024-09-18 14:21:51 +0300

    Fix outdatd <title> tag. Update .gitignore

--- public/schedule.php
+++ public/schedule.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['access_token'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>KSE Better Schedule</title>
+    <title>Easy KSE schedule</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
